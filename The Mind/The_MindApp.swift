899ce825d56733214153ff5b0caf77eb0d74917c
Yorick Juffer <oyjuffer@gmail.com>
2023-02-18 12:36:13 +0100

    Initial Commit

---
+++ The Mind/The_MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_MindApp.swift
+//  The Mind
+//
+//  Created by Lemon on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct The_MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
